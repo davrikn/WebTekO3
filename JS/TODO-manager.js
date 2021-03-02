c8e576bb6919ebe5d0a564da3cd5b144237065a3
David Rise Knotten <david_knotten@hotmail.no>
2021-03-02 21:04:10 +0100

    done for today

--- JS/TODO-manager.js
+++ JS/TODO-manager.js
@@ -8,21 +8,29 @@ function new_TODO() {
     let new_todo_finishbtn = document.createElement("input");
     let new_todo_row = document.createElement("tr");
     let new_todo = document.createElement("td");
+
+    //Creates the button that moves the to do to the finished column
     new_todo_finishbtn.type="button";
     new_todo_finishbtn.value="Finish this TODO";
     new_todo_finishbtn.setAttribute("onclick","done_TODO(this)");
 
-
+    //Creates the button that removes the to do from the site
     new_todo_deletebtn.type="button";
     new_todo_deletebtn.value="Delete this TODO";
     new_todo_deletebtn.setAttribute("onclick","remove_TODO(this)");
 
+    //Sets all the elements classes
+    new_todo_div.setAttribute("class", "TODO-div");
+    new_todo_row.setAttribute("class", "TODO-row");
+
+
+    //"Packs" all the elements into a new row
     new_todo.append(todo_text.value);
-    new_todo_row.append(new_todo);
-    new_todo_row.append(new_todo_deletebtn);
-    new_todo_row.append(new_todo_finishbtn);
-    new_todo_div.append(new_todo_row);
-    todo_table.appendChild(new_todo_div);
+    new_todo_div.append(new_todo);
+    new_todo_div.append(new_todo_deletebtn);
+    new_todo_div.append(new_todo_finishbtn);
+    new_todo_row.append(new_todo_div);
+    todo_table.appendChild(new_todo_row);
 }
 
 function remove_TODO(deletebtn) {
@@ -32,9 +40,9 @@ function remove_TODO(deletebtn) {
 
 function done_TODO(finishedbtn){
     console.log("dasdasda")
-    finished_TODO_div = finishedbtn.parentNode.parentNode;
+    finished_TODO_row = finishedbtn.parentNode.parentNode;
     let finished_table = document.getElementById("FINISHED-table");
-    finished_TODO_div.parentNode.removeChild(finished_TODO_div);
-    finished_table.append(finished_TODO_div);
+    finished_TODO_row.parentNode.removeChild(finished_TODO_row);
+    finished_table.append(finished_TODO_row);
 
 }
